c51451e5b3a8631419d0f7460f3e77f80ce05743
lilittonoyanc27-art <lilit.tonoyanc27@gmail.com>
2026-05-21 08:58:27 +0400

    Update TheorySection.tsx

--- TheorySection.tsx
+++ TheorySection.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { theoryData } from "../theory";
+import { theoryData } from "./theory";
 import { BookOpen, Star, Sparkles, HelpCircle, CheckCircle2 } from "lucide-react";
 import { motion } from "motion/react";
 
